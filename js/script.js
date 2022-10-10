e3957c5dddcb799cc38ef76b664f50befc57d26d
aiswarya-5 <111aishu111@gmail.com>
2022-10-10 16:16:25 +0530

    x

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-for (var i = 1; i <= 100; i++) {
-	document.getElementById(i).innerHTML = i;
-}
-
 var dice = ['<img src="image/1.png" alt="dice">', '<img src="image/2.png" alt="dice">', '<img src="image/3.png" alt="dice">', '<img src="image/4.png" alt="dice">', '<img src="image/5.png" alt="dice">', '<img src="image/6.png" alt="dice">'];
 
 var my_turn = 0;
@@ -15,15 +11,14 @@ function play() {
 	if (my_turn == 0) {
 		document.getElementById('snakeMsg').innerHTML="player1 turn";		
 		document.getElementById('snakeMsg').style.color="#fff"
-		document.getElementById('snakeMsg').style.border = "thick dotted #f00";
+		document.getElementById('snakeMsg').style.border = "thick dotted #FFCC00";
 		document.getElementById('snakeMsg').style.backgroundColor = 'transparent';
 		if (previousFirst > 0) {
 			if (100 - previousFirst < random_no + 1) {
 				my_turn = 1;
 				return;
 			} else {
-				document.getElementById((previousFirst).toString()).innerHTML = previousFirst;
-				document.getElementById((previousFirst).toString()).style.color = "#000";
+				document.getElementById((previousFirst).toString()).innerHTML = "";
 			}
 		}
 		document.getElementById((previousFirst + random_no + 1).toString()).innerHTML ="<i class='material-icons player'>person</i>";
@@ -52,8 +47,7 @@ function play() {
 				my_turn = 0;
 				return;
 			} else {
-				document.getElementById((previousSec).toString()).innerHTML = previousSec;
-				document.getElementById((previousSec).toString()).style.color = "#000";
+				document.getElementById((previousSec).toString()).innerHTML = "";
 			}
 		}
 		document.getElementById((previousSec + random_no + 1).toString()).innerHTML = "<i class='material-icons'>person</i>";
@@ -79,8 +73,7 @@ function play() {
 
 function snake(place_no, color_given, previous_player) {
 	if (place_no == 26) {
-		document.getElementById("26").innerHTML = 26;
-		document.getElementById("26").style.color = "#000";
+		document.getElementById("26").innerHTML = "";
 		document.getElementById('snakeMsg').innerHTML = "Sorry.. You are Swallowed by the snake <p> Better luck next time</p>";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#da291c";
@@ -90,8 +83,7 @@ function snake(place_no, color_given, previous_player) {
 		return 4;
 
 	} else if (place_no == 94) {
-		document.getElementById("94").innerHTML = 94;
-		document.getElementById("94").style.color = "#000";
+		document.getElementById("94").innerHTML = "";
 		document.getElementById('snakeMsg').innerHTML = "Sorry.. You are Swallowed by the snake <p> Better luck next time</p>";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#da291c";
@@ -100,8 +92,7 @@ function snake(place_no, color_given, previous_player) {
 		document.getElementById("18").style.color = color_given;
 		return 18;
 	} else if (place_no == 63) {
-		document.getElementById("63").innerHTML = 63;
-		document.getElementById("63").style.color = "#000";
+		document.getElementById("63").innerHTML = "";
 		document.getElementById('snakeMsg').innerHTML = "Sorry.. You are Swallowed by the snake <p> Better luck next time</p>";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#da291c";
@@ -110,8 +101,7 @@ function snake(place_no, color_given, previous_player) {
 		document.getElementById("21").style.color = color_given;
 		return 21;
 	} else if (place_no == 80) {
-		document.getElementById("80").innerHTML = 80;
-		document.getElementById("80").style.color = "#000";
+		document.getElementById("80").innerHTML ="";
 		document.getElementById('snakeMsg').innerHTML = "Sorry.. You are Swallowed by the snake <p> Better luck next time</p>";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#da291c";
@@ -120,8 +110,7 @@ function snake(place_no, color_given, previous_player) {
 		document.getElementById("58").style.color = color_given;
 		return 58;
 	} else if (place_no == 73) {
-		document.getElementById("73").innerHTML = 73;
-		document.getElementById("73").style.color = "#000";
+		document.getElementById("73").innerHTML ="";
 		document.getElementById('snakeMsg').innerHTML = "Sorry.. You are Swallowed by the snake <p> Better luck next time</p>";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#da291c";
@@ -130,8 +119,7 @@ function snake(place_no, color_given, previous_player) {
 		document.getElementById("50").style.color = color_given;
 		return 50;
 	} else if (place_no == 98) {
-		document.getElementById("98").innerHTML = 98;
-		document.getElementById("98").style.color = "#000";
+		document.getElementById("98").innerHTML = "";
 		document.getElementById('snakeMsg').innerHTML = "Sorry.. You are Swallowed by the snake <p> Better luck next time</p>";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#da291c";
@@ -150,8 +138,7 @@ function snake(place_no, color_given, previous_player) {
 function ladder(place_no, color_given, previous_player) {
 	if (place_no == 3) {
                 
-		document.getElementById("3").innerHTML = 3;
-		document.getElementById("3").style.color = "#000";
+		document.getElementById("3").innerHTML ="";
 		document.getElementById('snakeMsg').innerHTML = "Hurray!! You climbed the Ladder";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#04aa6d";
@@ -160,8 +147,7 @@ function ladder(place_no, color_given, previous_player) {
 		document.getElementById("24").style.color = color_given;
 		return 24;
 	} else if (place_no == 13) {
-		document.getElementById("13").innerHTML = 13;
-		document.getElementById("13").style.color = "#000";
+		document.getElementById("13").innerHTML = "";
 		document.getElementById('snakeMsg').innerHTML = "Hurray!! You climbed the Ladder";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#04aa6d";
@@ -170,8 +156,7 @@ function ladder(place_no, color_given, previous_player) {
 		document.getElementById("95").style.color = color_given;
 		return 95;
 	} else if (place_no == 12) {
-		document.getElementById("12").innerHTML = 12;
-		document.getElementById("12").style.color = "#000";
+		document.getElementById("12").innerHTML = "";
 		document.getElementById('snakeMsg').innerHTML = "Hurray!! You climbed the Ladder";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#04aa6d";
@@ -180,8 +165,7 @@ function ladder(place_no, color_given, previous_player) {
 		document.getElementById("52").style.color = color_given;
 		return 52;
 	} else if (place_no == 61) {
-		document.getElementById("61").innerHTML = 61;
-		document.getElementById("61").style.color = "#000";
+		document.getElementById("61").innerHTML ="";
 		document.getElementById('snakeMsg').innerHTML = "Hurray!! You climbed the Ladder";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#04aa6d";
@@ -190,8 +174,7 @@ function ladder(place_no, color_given, previous_player) {
 		document.getElementById("99").style.color = color_given;
 		return 99;
 	} else if (place_no == 72) {
-		document.getElementById("72").innerHTML = 72;
-		document.getElementById("72").style.color = "#000";
+		document.getElementById("72").innerHTML = "";
 		document.getElementById('snakeMsg').innerHTML = "Hurray!! You climbed the Ladder";		
 		document.getElementById('snakeMsg').style.color="#fff"
 		document.getElementById('snakeMsg').style.backgroundColor = "#04aa6d";
